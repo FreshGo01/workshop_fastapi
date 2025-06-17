857a49d0653ea91f15b5612c56dd3d75f7401acd
FreshGo01 <freshnathawat@gmail.com>
2025-06-17 15:29:30 +0700

    feat: crud items

--- main.py
+++ main.py
@@ -40,8 +40,12 @@ class ItemDto(BaseModel):
     price: float
 
 
-items: dict[int, Item] = {}
-id = 0
+items: dict[int, Item] = {
+    1 : Item(item_id=1, name="first item",price=10),
+    2 : Item(item_id=2, name="second item",price=20),
+    3 : Item(item_id=3, name="third item",price=30)
+}
+id = 3
 
 
 @app.post("/items/", response_model=Item, tags=["items"])
@@ -53,3 +57,27 @@ def create_item(item: ItemDto):
         item_id=id, name=item.name, description=item.description, price=item.price
     )
     return items[id]
+
+@app.get("/items", response_model=list[Item], tags=["items"])
+def get_all_items():
+    return [item for item in items.values()]
+
+@app.get("/items/{item_id}", response_model=Item|dict, tags=["items"])
+def get_one_items(item_id: int):
+    if item_id not in items:
+        return {"message": "item not found"}
+    return items[item_id]
+
+@app.put("/items/{item_id}", response_model=Item | dict, tags=["items"])
+def update_item(item_id: int, item: ItemDto):
+    if item_id not in items:
+        return {"message": "item not found"}
+    items[item_id] = Item(item_id=item_id,name=item.name,description=item.description,price=item.price)
+    return items[item_id]
+
+@app.delete("/items/{item_id}", response_model=dict, tags=["items"])
+def delete_item(item_id: int):
+    if item_id not in items:
+        return {"message": "item not found"}
+    del items[item_id]
+    return {"message": "delete success"}
